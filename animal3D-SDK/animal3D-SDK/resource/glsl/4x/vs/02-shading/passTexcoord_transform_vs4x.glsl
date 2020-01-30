8051f58300f2b17ae612abbe503473fa62464dd5
Nick <dacosta.nick@yahoo.com>
2020-01-30 00:32:00 -0500

    Completed 2-4 Vertex Shader

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passTexcoord_transform_vs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passTexcoord_transform_vs4x.glsl
@@ -28,18 +28,37 @@
 // ****TO-DO: 
 //	--1) declare uniform variable for MVP matrix; see demo code for hint
 //	--2) correctly transform input position by MVP matrix
-//	3) declare texture coordinate attribute; see graphics library for location
-//	4) declare atlas transform; see demo code for hint
-//	5) declare texture coordinate outbound varying
-//	6) correctly transform input texture coordinate by atlas matrix
+//	--3) declare texture coordinate attribute; see graphics library for location
+//	--4) declare atlas transform; see demo code for hint
+//	--5) declare texture coordinate outbound varying
+//	--6) correctly transform input texture coordinate by atlas matrix
 
 layout (location = 0) in vec4 aPosition;
+
+// ***NOTE*** Attributes always have a vec4 allocation.
+
+// Attribute input texure coordinate.
+layout (location = 8) in vec4 aTextureCoordinate;
 
 // Model View Projection matrix.
 uniform mat4 uMVP;
 
+uniform mat4 uAtlas;
+
+// Varying output texture coordinate.
+out vec4 vTextureCoordinate;
+
+
 void main()
 {
+	// DUMMY OUTPUT: directly assign input position to output position
+	//gl_Position = aPosition;
+
 	// Assign input position modified by MVP to output position.
 	gl_Position = uMVP * aPosition;
+
+	//vTextureCoordinate = vec2(uAtlas * aTextureCoordinate);
+	vTextureCoordinate = uAtlas * aTextureCoordinate;
+
+
 }
